7f31310f015931abe8c450c1299f8d7759aa8c6c
Rajashree Sonawane <rajashree.sonawane@encora.com>
2024-02-07 17:35:41 +0530

    Cucumber Hooks project

--- CucumberHooks/src/test/java/LoginHooks/loginSteps.java
+++ CucumberHooks/src/test/java/LoginHooks/loginSteps.java
@@ -1,6 +1,7 @@
 package LoginHooks;
 import java.util.concurrent.TimeUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,18 +18,15 @@ import io.cucumber.java.en.When;
 public class loginSteps
 {
 
-	WebDriver driver=null;
+	WebDriver driver;
 	@Before("@smoke")
 	public void browser_setup() {
-		String projectPath=System.getProperty("user.dir");
-		   System.out.println("Project path is: "+projectPath);
-		   System.setProperty("webdriver.chrome.driver","C:/Users/rajashrees/eclipse-workspace/CucumberHooks/src/test/resources/drivers/chromedriver.exe");
-		   driver=new ChromeDriver();
-		   
-		   driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
-		   driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
-		   driver.manage().window().maximize();
-		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
 	}
 	@Before(order=0)
 	public void setup2()
@@ -39,8 +37,8 @@ public class loginSteps
 	public void teardown()
 	{
 		System.out.println("I am inside teardown \n");
-		driver.close();
-		driver.quit();
+		//driver.close();
+		//driver.quit();
 	}
 	@After(order=0)
 	public void teardown2()
@@ -57,42 +55,42 @@ public class loginSteps
 	{
 		System.out.println("I am inside after step  \n");
 	}
-	
+
 	@Given("browser is open")
 	public void browser_is_open() {
-	   System.out.println("inside step-browser is open");
-	  
-}
-	
-	
+		System.out.println("inside step-browser is open");
+
+	}
+
+
 
 	@And("user is on login page")
 	public void user_is_on_login_page() {
-	   System.out.println("inside Step - user is on login page");
-	   driver.navigate().to("https://login.salesforce.com/?locale=in");
+		System.out.println("inside Step - user is on login page");
+		driver.navigate().to("https://login.salesforce.com/?locale=in");
 	}
 
 	@When("user enters username and password")
 	public void user_enters_username_and_password() throws InterruptedException {
-	    System.out.println("inside step- user enter username and password");
-	    driver.findElement(By.xpath("//input[@id='username']")).sendKeys("Abc@123");
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Kraj@123");
-	    Thread.sleep(2000);
+		System.out.println("inside step- user enter username and password");
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("Abc@123");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Kraj@123");
+		Thread.sleep(2000);
 	}
 
 	@And("clicks on login button")
 	public void clicks_on_login_button() throws InterruptedException {
 		System.out.println("inside step- clicks on login button");
 		driver.findElement(By.xpath("//input[@id='Login']")).click();
-		 Thread.sleep(2000);
-	   
+		Thread.sleep(2000);
+
 	}
 
 	@Then("user should be display my login page")
 	public void user_is_navigated_to_the_home_page() {
 		System.out.println("inside step-should display my login page");
-		
+
 	}
 
 }
